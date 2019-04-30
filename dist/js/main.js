cd96eff723420e2ac1921b46ea739c958c69d4bf
Chris Silich <chris.silich@gmail.com>
2019-04-30 11:20:25 -0400

    js ready

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+// Uses AJAX to request a new Chuck Norris fact from the Chuck Norris API.
+// The API responds with an object, and that object contains the fact, among other things
+// We then call showFact with that object
+// You shouldn't need to modify this function, just call it when you need a new fact,
+// and then modify the showFact function to make it do what you want with the data.
+var getFact = function getFact() {
+  var CHUCK_API = 'https://api.chucknorris.io/jokes/random';
+  factElement.innerHTML = '';
+  axios.get(CHUCK_API).then(function (response) {
+    // chuckData = response;
+    showFact(response.data);
+  }).catch(function (error) {
+    console.warn(error);
+  });
+};
+
+var showFact = function showFact(data) {
+  console.log(data);
+};
 //# sourceMappingURL=main.js.map
